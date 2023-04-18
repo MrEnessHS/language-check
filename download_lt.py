d2d49f37fbe491a53a95a44572942a4f9835273f
MrEnessHS <76776068+MrEnessHS@users.noreply.github.com>
2023-04-18 17:32:07 +0300

    Update download_lt.py

--- download_lt.py
+++ download_lt.py
@@ -6,10 +6,12 @@ import os
 if __name__ == '__main__':
     if not os.name == 'posix':
         raise Exception('This fork is only for Linux')
-    if not os.path.exists('/usr/bin/curl'):
-        raise Exception('Curl is not installed')
+    if not os.path.exists('/usr/bin/wget'):
+        raise Exception('Wget is not installed')
     if not os.path.exists('/usr/bin/unzip'):
         raise Exception('Unzip is not installed')
-    os.system("curl https://www.languagetool.org/download/LanguageTool-3.2.zip -o LanguageTool-3.2.zip")
+    if not os.path.exists('LanguageTool-3.2.zip")
+        os.system("wget https://www.languagetool.org/download/LanguageTool-3.2.zip")
+    os.system("rm -rf LanguageTool-3.2")
     os.system("unzip LanguageTool-3.2.zip")
     os.system("cp -r LanguageTool-3.2 language_tool")
